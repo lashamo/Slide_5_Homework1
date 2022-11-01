28ea33c0cf35831743bffdb3b152d2ae84ce1ea7
Lasha Modzmanashvili <shishtviliba@gmail.com>
2022-11-01 13:38:24 +0400

    https://github.com/lashamo/Slide_5_Homework1.git

---
+++ src/com/learn/slide5/Person.java
@@ -0,0 +1,67 @@
+package com.learn.slide5;
+
+public class Person {
+    private String identityNumber;
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+    }
+
+    public Person(String identityNumber, String firstName, String lastName) {
+        this.identityNumber = identityNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getIdentityNumber() {
+        return identityNumber;
+    }
+
+    public void setIdentityNumber(String identityNumber) {
+        this.identityNumber = identityNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        Person person = (Person) obj;
+        return this.identityNumber.equals(person.getIdentityNumber());
+    }
+
+    @Override
+    public int hashCode() {
+        return identityNumber.hashCode() * firstName.hashCode() * lastName.hashCode();
+    }
+
+    @Override
+    public String toString(){
+        return identityNumber + "  " + firstName + "  " + lastName;
+    }
+
+
+}
